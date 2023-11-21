43bd2d780d675b2294a7a82ff58ebc012cfaa1da
Thomas Suphona <thomas.suphona@gmail.com>
2023-11-21 10:50:37 +0100

    Implement SDL project and update cmake config
    Implement first interation of the new SDL project
    and update c++ standard to C++20 and update CMake
    minimun required version to 3.27.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include "modules/module_1/include/module_1.hpp"
-#include "modules/module_2/include/module_2.hpp"
+#include "module_1.hpp"
+#include "module_2.hpp"
+#include <SDL2/SDL.h>
 #include <list>
 #include <vector>
 
@@ -19,5 +20,56 @@ int main() {
   bool found;
   found = binary_search(&v, 22);
   std::cout << found << std::endl;
+
+  // SDL2 project.
+  // SDL Init stuff.
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Window *window = nullptr;
+  SDL_Renderer *renderer = nullptr;
+  SDL_CreateWindowAndRenderer(2000, 2000, 0, &window, &renderer);
+  SDL_Event event;
+
+  // A single point to update every frame.
+  SDL_Point current;
+  std::vector<SDL_Point> vector_of_points;
+
+  // Main loop.
+  while (true) {
+
+    // Events.
+    while (SDL_PollEvent(&event)) {
+      // Handle closing the window.
+      if (event.type == SDL_QUIT) {
+        SDL_Quit();
+        exit(0);
+      }
+      // Store the current location of the mouse if we click on the mouse.
+      if (event.type == SDL_MOUSEBUTTONDOWN) {
+        int x, y;
+        auto button = SDL_GetMouseState(&x, &y);
+
+        if (event.button.button == SDL_BUTTON_LEFT) {
+          vector_of_points.emplace_back(SDL_Point{x, y});
+        }
+      }
+      // Get the current mouse location per frame.
+      if (event.type == SDL_MOUSEMOTION) {
+        SDL_GetMouseState(&current.x, &current.y);
+      }
+      // If the c key is clicked, we are going to clear the container.
+      if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_c) {
+        vector_of_points.clear();
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDrawLines(renderer, vector_of_points.data(),
+                        vector_of_points.size());
+    SDL_RenderPresent(renderer);
+    SDL_Delay(150);
+  }
+
   return 0;
 }
