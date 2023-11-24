3a0421ef1ade28894490fe6fc8df32dfa733a713
Thomas Suphona <thomas.suphona@gmail.com>
2023-11-24 15:57:36 +0100

    Implement glad and glfw

--- modules/opengl/src/opengl.cpp
+++ modules/opengl/src/opengl.cpp
@@ -7,14 +7,16 @@
 int main(int argc, char* argv[]) {
   // Initialize GLFW
   if (!glfwInit()) {
-    // Initialization failed
+    std::cerr << "Failed to initialize GLFW" << std::endl;
     return -1;
   }
 
-  // Create a windowed mode window and its OpenGL context
-  GLFWwindow* window = glfwCreateWindow(640, 480, "Hello GLFW", NULL, NULL);
-  if (!window) {
+  // Create a GLFW window object.
+  GLFWwindow* window = glfwCreateWindow(640, 480, "Hello GLFW, OpenGl and GLAD",
+                                        nullptr, nullptr);
+  if (window == nullptr) {
     // Window or OpenGL context creation failed
+    std::cerr << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
     return -1;
   }
@@ -22,17 +24,30 @@ int main(int argc, char* argv[]) {
   // Make the window's context current
   glfwMakeContextCurrent(window);
 
+  // Load all OpenGL function pointers with GLAD
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "Failed to initialize GLAD" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  // Set the viewport
+  glViewport(0, 0, 640, 480);
+
   // Main loop
   while (!glfwWindowShouldClose(window)) {
-    // Render here
+    // Poll for and process events
+    glfwPollEvents();
+
+    // Rendering commands here.
+    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
     // Swap front and back buffers
     glfwSwapBuffers(window);
-
-    // Poll for and process events
-    glfwPollEvents();
   }
 
+  // Clean up and exit.
   glfwTerminate();
   return 0;
 }
